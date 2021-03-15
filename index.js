612b7698c00b64d8c048f458da21755749394fcf
BuiXuanSang <hainam4536@gmail.com>
2021-03-15 22:16:24 +0700

    add file gitignore

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ console.log("git commit -m "abc");
 
 console.log("git push origin master");
 
-console.log("git reset HEAD ||--soft: quay lai luc add roi||--mixed:quay lai luc chua add||--hard");
+console.log("git reset HEAD ||--soft: quay lai luc add roi||--mixed:quay lai luc chua add||--hard:xoa tat ca thay doigit ");
 
 console.log('123456');
 
-wsjvsv232344git addEventL
-
-matchMedia,mm
+.gitignone : loai bo nhung file ma minh khong muon uplaod len
