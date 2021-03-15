7907ca8c1568254dffcc99be89856dafdc52baa4
BuiXuanSang <hainam4536@gmail.com>
2021-03-15 22:01:18 +0700

    change console.log line 6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("git commit -m "abc");
 
 console.log("git push origin master");
 
-console.log("yeeeeee");
+console.log("git reset HEAD ||--soft: quay lai luc add roi||--mixed:quay lai luc chua add||--hard");
 
 console.log('123456');
 
